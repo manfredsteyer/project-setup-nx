d000051778c5f7b6082a78dac4e98bd55d7971b2
Manfred Steyer <manfred.steyer@gmx.net>
2025-03-29 14:40:39 +0100

    feat: add authentication

--- apps/flights/src/app/app.component.ts
+++ apps/flights/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { UtilAuthComponent } from '@my-project/util-auth';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, UtilAuthComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
